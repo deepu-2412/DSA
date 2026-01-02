4d60501c8abeb97acd02280fd7f7b155d560c1f5
deepu-2412 <142926240+deepu-2412@users.noreply.github.com>
2026-01-02 15:12:26 +0530

    Time: 131 ms (47.32%) | Memory: 18.1 MB (38.75%) - LeetSync

---
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
@@ -0,0 +1,16 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        l = 0;r=0;ml=0;mf=0
+        c = [0]*26
+        while r<len(s):
+            c[ord(s[r])-ord('A')]+=1
+            mf = max(mf,c[ord(s[r])-ord('A')])
+
+            if (r-l+1) - mf > k:
+                c[ord(s[l]) - ord('A')] -=1
+                mf = 0
+                l = l+1
+            if (r-l+1)-mf<=k:
+                ml = max(ml,r-l+1)
+            r = r+1
+        return ml 
